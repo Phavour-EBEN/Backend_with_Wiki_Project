97c95312d834d69d6b3a8c5bc14b269566148374
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-10-05 20:41:29 +0000

    Simple hello world project

--- Assignment.js
+++ Assignment.js
@@ -4,8 +4,19 @@
 // ○ Make at least three meaningful commits throughout the project. 
 // ○ Push the repository to GitHub and submit the link for review.
 
-console.log("Hello world")
+console.log("Hello world");
 
-// function print_fun(){
-//     for(let i = 0;)
-// }
+var statement= "Hello world";
+
+function print_fun(statement){
+   
+    for(let i = 0; i<statement.length; i++){
+        
+        console.log(statement[i])
+    }
+}
+// print_fun(statement);
+
+for(let x of statement){
+    console.log(x)
+}
