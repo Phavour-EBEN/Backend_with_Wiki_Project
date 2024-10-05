7b06ef78feb189221d1516f453f932e2f318e231
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-10-05 20:27:00 +0000

    Simple hello world project

---
+++ Assignment.js
@@ -0,0 +1,11 @@
+// Coding Assignment: 
+// ○Create a Git repository for a small project 
+// ○ Create a simple "Hello World" project and push it to your GitHub repo. 
+// ○ Make at least three meaningful commits throughout the project. 
+// ○ Push the repository to GitHub and submit the link for review.
+
+Console.log("Hello world")
+
+// function print_fun(){
+//     for(let i = 0;)
+// }
